6a6663c145fac262f0c42a2153b38116b2e7610f
nash1111 <ken1251213@gmail.com>
2023-07-18 21:49:41 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-use chrono::{NaiveDateTime, Datelike, Utc, Weekday, DateTime};
-use serde::{Serialize, Deserialize};
+use chrono::{DateTime, Datelike, NaiveDateTime, Utc, Weekday, NaiveTime};
+use chrono::Timelike;
+use serde::{Deserialize, Serialize};
 use worker::*;
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -9,29 +10,40 @@ pub struct UnixTimePayload {
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
-    //Response::ok("test".to_string())
-    if req.method() != Method::Post {
+    if req.method() != Method::Get {
         return Response::ok("{\"error\": \"Method Not Allowed\"}".to_string());
     }
-    //Response::ok("test".to_string())
     let full_path = req.path();
     let unixtime_str = full_path.trim_start_matches('/');
     let unixtime: i64 = unixtime_str.parse().unwrap();
 
-    let naive = NaiveDateTime::from_timestamp(unixtime, 0);
+    let naive = NaiveDateTime::from_timestamp_opt(unixtime, 0).unwrap();
     let datetime = DateTime::<Utc>::from_utc(naive, Utc);
+    let naive_time = datetime.time();
 
     let day = match datetime.weekday() {
-        Weekday::Mon => "月曜日",
-        Weekday::Tue => "火曜日",
-        Weekday::Wed => "水曜日",
-        Weekday::Thu => "木曜日",
-        Weekday::Fri => "金曜日",
-        Weekday::Sat => "土曜日",
-        Weekday::Sun => "日曜日",
+        Weekday::Mon => "月",
+        Weekday::Tue => "火",
+        Weekday::Wed => "水",
+        Weekday::Thu => "木",
+        Weekday::Fri => "金",
+        Weekday::Sat => "土",
+        Weekday::Sun => "日",
     };
 
-    let date_str = format!("{}/{}/{}({})", datetime.year(), datetime.month(), datetime.day(), day);
+    let hours = naive_time.hour();
+    let minutes = naive_time.minute();
+    let seconds = naive_time.second();
+
+
+    let date_str = format!("{}/{}/{}({}) {:02}:{:02}:{:02}", 
+                           datetime.year(), 
+                           datetime.month(), 
+                           datetime.day(), 
+                           day, 
+                           hours, 
+                           minutes, 
+                           seconds);
 
     Response::ok(date_str)
 }
